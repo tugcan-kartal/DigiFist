760e767f5ff4cfa14307d08121f8a07cfcfd8f39
tugcan-kartal <tugcankartal01@gmail.com>
2023-04-15 16:21:59 +0300

    before slider

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -14,23 +14,23 @@ const HomePage = () => {
                 <img className='w-[720px] h-[864px]' src={Girl} alt='not found'/>
             </div>
 
-            <div className='w-[720px] h-[864px] border-box'>
+            <div className='right-section w-[720px] h-[864px] border-box'>
                 <div className='ml-[120px] mt-[96px] mb-[92px]'>
 
                     <div className='textone w-[494px] h-[123px] mb-[12px] font-bold text-[31.25px] leading-[130%] items-end'>
                         Everyday items, we have something to suit every occasion.
                     </div>
 
-                    <div>
+                    <div className='texttwo w-[432px] h-[52px] mb-[32px] font-normal text-[16px] leading-[160%]'>
                         At suspendisse augue lectus arcu, accumsan ut sit posuere vitae sit tincidunt semper eu proin leo gravida cursus.
                     </div>
 
-                    <div>
+                    <div className='textthree w-[146px h-[17px] mb-[5px] font-black text-[12.8px] leading-[17px] items-center capitalize'>
                         Shop all everyday items
                     </div>
 
-                    <div>
-                        -------
+                    <div className='line w-[146px] h-[1px] mb-[68px]'>
+                        
                     </div>
 
                     <div>
